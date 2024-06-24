e69901ad0141392130b0465a41fed9114620e693
Ruthwikdj18 <135842791+Ruthwikdj18@users.noreply.github.com>
2024-06-24 21:46:46 +0530

    Add files via upload

---
+++ PWS/HTML/developers.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+        <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Project Submission System</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+        <link rel="stylesheet" type="text/css" href="../css/developers.css">
+ <title>Cards</title>
+</head>
+<body bgcolor="#5539cc">
+  <header>
+      <img src="../pics/clg.png" class="logo">
+      <nav>
+          <b><ul class="nav-area">
+              <li><a href="../HTML/main.php"><i class="fa fa-home"></i>&nbsp;HOME&nbsp;</a></li>
+              <li><a href="../HTML/perks.php"><i class="fa fa-medal"></i>&nbsp;PERKS&nbsp;</a></li>
+              <li><a href="../HTML/developers.php"><i class="fa fa-user"></i>DEVELOPERS</a></li>
+          </ul></b>
+      </nav>
+      <a href="../HTML/login.php" class="btn-area">Login</a>
+  </header>
+  <br><br>
+
+<body>
+
+ <div class="main">
+
+ <!--cards -->
+
+<div class="card">
+
+<div class="image">
+   <img src="../images/avatar.jpg">
+</div>
+<div class="title">
+ <h1>Ruthwik DJ</h1>
+</div>
+<div class="des">
+  <br>
+ <p><b>DEVELOPER</b></p>
+<button><a href="mailto:ruthwikgowda8@gmail.com"><i class="fa fa-envelope"></i>&nbsp;E-mail</a></button>
+<button><a href="" target="blank"><i class="fa fa-pencil"></i>Instagram</a></button>
+</div>
+</div>
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <img src="../images/avatar.jpg">
+</div>
+<div class="title">
+ <h1>Sandesh HN</h1>
+</div>
+<div class="des">
+  <br>
+ <p>DEVELOPER</p>
+ <button><a href="mailto:sandeshgowda020@gmail.com"><i class="fa fa-envelope"></i>&nbsp;E-mail</a></button>
+ <button><a href="" target="blank"><i class="fa fa-camera"></i>&nbsp;Instagram</a></button>
+</div>
+</div>
+<!--cards -->
+
+</div>
+</style>
+</body>
+</html>
